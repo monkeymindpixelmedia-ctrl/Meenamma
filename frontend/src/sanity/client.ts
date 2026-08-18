d85852c9fca89da4f483b0ae2ccd88f5ded6edc5
Dr. Senz Medical <rathnavelkarthi1@gmail.com>
2026-08-18 23:28:13 +0530

    chore: remove studio-mennama CMS directory and unused sanity dependencies

--- frontend/src/sanity/client.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@sanity/client'
-
-export const client = createClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID || 'aqwc55lo',
-  dataset: process.env.REACT_APP_SANITY_DATASET || 'production',
-  apiVersion: '2024-08-13',
-  useCdn: false,
-  token: process.env.REACT_APP_SANITY_API_READ_TOKEN,
-})
